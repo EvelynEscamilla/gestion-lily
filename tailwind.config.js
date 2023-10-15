c5b64adf55fd62c66dec8932385ecf2148bf8b6a
EvelynEscamilla <shieunwook@gmail.com>
2023-10-14 21:14:51 -0600

    configuracion colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],  theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        'glow': '0 0 10px rgba(11, 216, 28, 1)'
+      },
+      colors: {
+        'azulNav': '#EDF8F8',
+        'azul': '#68D4D8',
+        'turqueza': '#41A6BA',
+        'azulClaro': '#93CED6',
+        'morado':'#8893c2'
+
+      },
+    },
+    fontFamily: {
+      'roboto': 'Roboto, sans-serif'
+    }
   },
   plugins: [],
 }
