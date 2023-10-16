94d1749161652eea75729ef840feb0ea9eb3dfe9
EvelynEscamilla <shieunwook@gmail.com>
2023-10-16 17:53:59 -0600

    correcion tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
- navbar  content: [
+  content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
     ],  theme: {
@@ -24,28 +24,3 @@ module.exports = {
     plugins: [],
   }
   
-=======
-content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
-    extend: {
-      dropShadow: {
-        'glow': '0 0 10px rgba(11, 216, 28, 1)'
-      },
-      colors: {
-        'azulNav': '#EDF8F8',
-        'azul': '#68D4D8',
-        'turqueza': '#41A6BA',
-        'azulClaro': '#93CED6',
-        'morado':'#8893c2'
-
-      },
-    },
-    fontFamily: {
-      'roboto': 'Roboto, sans-serif'
-    }
-  },
-  plugins: [],
-}
-
