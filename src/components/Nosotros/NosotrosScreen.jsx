5fcd2de6ea53731f84a2a9516399133b13cb3e95
Rodrik-09 <becerra24502@gmail.com>
2023-10-16 12:05:10 -0600

    pantalla nosotros terminada

---
+++ src/components/Nosotros/NosotrosScreen.jsx
@@ -0,0 +1,45 @@
+import "./NosotrosScreen.css"
+
+export const NosotrosScreen = () => {
+    return (
+        <>
+        
+            
+                <div className="InfoComponent m-24 mt-8">
+                    <div className="LabelCont bg-turqueza w-72 h-12 rounded-r-lg text-center pt-1">
+                        <label className="text-white text-3xl font-medium ">Quiénes Somos</label>
+                    </div>
+                    <div className="divInfo w-full h-96 mt-8">
+                        <p className="parrafo1 absolute font-semibold mt-6 ml-5 text-lg w-8/12">
+                            La clínica de belleza Lily es un negocio dedicado a brindar tratamientos corporales y faciales...
+                        </p>
+                        <img src="/gestion-lily/src/assets/spa1.png" className="info h-full w-full object-cover  rounded-3xl" />
+                    </div>
+                </div>
+
+                <div className="InfoComponent m-24 mt-8">
+                    <div className="LabelCont bg-turqueza w-72 h-12 rounded-r-lg text-center pt-1">
+                        <label className="text-white text-3xl font-medium ">Historia</label>
+                    </div>
+                    <div className="divInfo w-full h-96 mt-8">
+                        <p className="parrafo1 absolute font-semibold mt-6 ml-5 text-lg w-8/12">
+                            La clínica de belleza Lily se creó a partir de...
+                        </p>
+                        <img src="/gestion-lily/src/assets/spa2.jpg" className="info h-full w-full object-cover  rounded-3xl" />
+                    </div>
+                </div>
+
+                <div className="InfoComponent m-24 mt-8">
+                    <div className="LabelCont bg-turqueza w-72 h-12 rounded-r-lg text-center pt-1">
+                        <label className="text-white text-3xl font-medium ">Instalaciones</label>
+                    </div>
+                    <div className="divInfo w-full h-full mt-8">
+                        
+                        <img src="/gestion-lily/src/assets/clinica.jpg" className="info h-full w-full object-cover rounded-3xl" />
+                        <img src="/gestion-lily/src/assets/clinica1.jpg" className="info h-full w-full object-cover mt-8 rounded-3xl" />
+                    </div>
+                </div>
+                
+        </>
+    )
+}
