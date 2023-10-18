2e8291eab111ec0cffd67e575f63b2ed9ba40bb9
Omar Garcia Alvarez <l20120988@morelia.tecnm.mx>
2023-10-17 19:16:33 -0600

    Mejora todavia mejor

--- src/Components/Galeria/Galeria.jsx
+++ src/Components/Galeria/Galeria.jsx
@@ -1,12 +1,16 @@
 import Servicios from "../Servicios/Servicios";
-function Galeria({ imagenes }) {
+import imagenes from "../Imagenes/Imagenes";
+function Galeria({EnviarDatos}) {
+  const handleDFC = (data) => {
+    console.log('Datos recibidos desde Galeria', data)
+  }
   return (
     <div className=" grid grid-cols-3 place-content-center py-2">
       {imagenes.map((item, index) => (
         <Servicios
           key={index}
-          nombre={item.nombre}
-          imagen={item.imagen}
+          sendDataToParent={handleDFC}
+          item={item}
         ></Servicios>
       ))}
     </div>
