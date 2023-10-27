5e2c6046559919eb9a3c1374dd24ba8d95c7f6a9
AngheloX09 <linkg4f@gmail.com>
2023-10-27 17:28:57 -0600

    Ya le puse tailwind, pero no quiten el css

--- src/Components/CrearCuenta/CrearCuenta.jsx
+++ src/Components/CrearCuenta/CrearCuenta.jsx
@@ -6,58 +6,48 @@ import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
 
 
-const CrearCuenta = ({handleScreen}) => {
-  return (
-    <>
-    <Navbar handleScreen={handleScreen}/>
-     <div className="container">
-                <div className="image-container">
-                    <img src={Wpr} alt="Descripción de la imagen"></img>
-                    <div className="text-overlay">
-                        <br></br>
-                        <div className='flex justify-center'>
-                        <img src={LogoHor} alt="Logo Lily" className="logo"></img>
-                        </div>
-                        <br></br>
-                        <br></br>
-                        <br></br>
+const CrearCuenta = ({ handleScreen }) => {
+    return (
+        <>
+            <Navbar handleScreen={handleScreen} />
+            <div className="container flex justify-center items-center w-auto h-80% ">
+                <div className="relative overflow-hidden w-auto h-80%">
+                    <img src={Wpr} alt="Descripción de la imagen" className="w-full h-auto"></img>
+                    <div className="text-overlay absolute top-0 right-0 bottom-0 w-1/2 bg-opacity-70 bg-blue-100 text-black p-1 text-center flex flex-col justify-center items-center">
+                        <img className="w-1/2 mx-auto py-7 " src={LogoHor}></img>
                         <div className="texto-fuente2">
-                            <p>
-                            Crea una cuenta en nuestro sitio
-                            </p>
+                            <p>Crea una cuenta en nuestro sitio</p>
                         </div>
-                        <br></br>
-                        <br></br>
-                        <form>
-                            <input className="datos" type="text" placeholder="Correo electronico"></input>
-                            <br></br>
-                            <br></br>
-                            <input className="datos" type="text" placeholder="Contraseña"></input>
-                            <br></br>
-                            <br></br>
-                            <input className="datos" type="text" placeholder="Confirmar Contraseña"></input>
-                            <br />
-                            <br />
+                        <form className="text-center">
+                            <input className="datos w-3/4 h-10 rounded-full mb-4" type="text" placeholder="Correo electrónico"></input>
+                            <input className="datos w-3/4 h-10 rounded-full mb-4" type="text" placeholder="Contraseña"></input>
+                            <input className="datos w-3/4 h-10 rounded-full mb-4" type="text" placeholder="Confirmar Contraseña"></input>
                             <p className="texto-fuente">
-                            Utiliza minimo ocho caracteres combinados
+                                Utiliza mínimo ocho caracteres combinados
                             </p>
                             <p className="texto-fuente">
-                            Letras, números y símbolos
+                                Letras, números y símbolos
                             </p>
-                            <br />
-                            <br />
-                            <br />
-                            <p className="texto-fuente3">
-                                <input type="submit" value="Cancelar" className="custom-submit3 bg-morado"/>
-                                <input type="submit" value="Crear Cuenta" className="custom-submit2 bg-morado"/>
-                            </p>
+                            <div className="mt-4">
+                                <input
+                                    type="submit"
+                                    value="Cancelar"
+                                    className="custom-submit2 bg-morado m-2 rounded-full cursor-pointer float-left"
+                                ></input>
+                                <input
+                                    type="submit"
+                                    value="Crear Cuenta"
+                                    className="custom-submit2 bg-morado m-2 rounded-full cursor-pointer float-right"
+                                ></input>
+                            </div>
                         </form>
                     </div>
                 </div>
             </div>
-            <Footer/>
-    </>
-  )
+
+            <Footer />
+        </>
+    )
 }
 
 export default CrearCuenta
