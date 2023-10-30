1d7bea14fb85d5d5341d205c4d0f6b26884dd3b6
AngheloX09 <linkg4f@gmail.com>
2023-10-30 12:35:07 -0600

    Aun le falta poquito, mas tarde lo termino

---
+++ src/Components/PerfilCliente/PerfilCliente.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import Perfil from '../../assets/logosolito.svg'
+
+export const PerfilCliente = ({handleScreen}) => {
+    return (
+        <>
+            <Navbar handleScreen={handleScreen}/>
+            <body className="bg-gray-100 flex justify-center items-center h-screen">
+                <div className="bg-teal-100 rounded-lg p-4 shadow-lg items-center">
+                    <div className='flex items-center p-4'>
+                    <div>
+                        <img src={Perfil} className="w-32 h-32" alt="Imagen de perfil" />
+                    </div>
+                    <div className="ml-4">
+                        <p className="text-xl font-bold">Nombre de Usuario</p>
+                    </div>
+                    </div>
+                    <br></br>
+                    <p className="text-xl font-bold">Correo electronico:</p>
+                    <p>Correo@gmail.com</p>
+                    <br></br>
+                    <p className="text-xl font-bold">Numero de telefono:</p>
+                    <p>4412345678</p>
+                </div>
+            </body>
+            <Footer />
+        </>
+    )
+}
