e20ec040cb1783e5b84bbc385a0840fbe616c94f
AngheloX09 <linkg4f@gmail.com>
2023-11-01 21:00:10 -0600

    Ya estuvo

--- src/Components/PerfilCliente/PerfilCliente.jsx
+++ src/Components/PerfilCliente/PerfilCliente.jsx
@@ -11,21 +11,31 @@ export const PerfilCliente = ({ handleScreen }) => {
                 <div className="bg-teal-100 rounded-lg p-4 shadow-lg items-center">
                     <div className='flex items-center p-4'>
                         <div>
-                            <img src={Perfil} className="w-48 h-48 rounded-full" alt="Imagen de perfil" />
+                            <img src={Perfil} className="hover:-translate-y-1 hover:scale-110 duration-200 border-[5px] border-morado w-64 h-64 rounded-full" alt="Imagen de perfil" />
                         </div>
-                        <div className="ml-4">
-                            <p className="text-xl font-bold">Nombre de Usuario</p>
+                        <div className="ml-4 text-white bg-morado px-5 py-2 rounded-md">
+                            <div className='py-3'>
+                            <p className="text-xl   font-bold">Nombre de Usuario Cool</p>
+                            </div>
+                            <div className='py-3'>
+                            <span className=" mr-2 text-xl font-bold">Correo electronico: </span>
+                            <span className="  text-xl ">CorreoElectronico@Gmail.com</span>
+                            </div>
+                            <div className='py-3'>
+                            <span className="  mr-2 text-xl font-bold">Telefono: </span>
+                            <span className=" text-xl">4412345678</span>
+                            </div>
                         </div>
                     </div>
                     <br></br>
-                    <p className="text-xl font-bold">Correo electronico:</p>
-                    <p>Correo@gmail.com</p>
                     <br></br>
-                    <p className="text-xl font-bold">Numero de telefono:</p>
-                    <p>4412345678</p>
-                    <br></br>
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded-full hover:rounded-lg inset-y-0 left-0 ">Editar</button>
-                    <button className="bg-red-500 text-white px-4 py-2 rounded-full mr-4 hover:rounded-lg inset-y-0 right-0 ">Eliminar cuenta</button>
+                    <button className="bg-blue-500 text-white px-4 py-2 rounded-full hover:rounded-lg inset-y-0 left-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                        </svg>
+                    </button>
+
+
                 </div>
             </body>
             <Footer />
