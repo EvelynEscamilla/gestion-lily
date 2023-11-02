334d88bd524d44864d7751f4390db646d36a650f
Omar Garcia Alvarez <l20120988@morelia.tecnm.mx>
2023-11-01 18:32:24 -0600

    Ahora si terminado

--- src/Components/GestionarServicios/AgregarServicio.jsx
+++ src/Components/GestionarServicios/AgregarServicio.jsx
@@ -5,8 +5,10 @@ import Footer from "../Footer/Footer";
 import logo from "../../assets/logosolito.svg";
 
 const AgregarServicio = ({ handleScreen }) => {
+
   return (
     <>
+      <Navbar handleScreen={handleScreen}></Navbar>
       <div>
         <section className="p-6  text-black">
           <form
@@ -14,14 +16,15 @@ const AgregarServicio = ({ handleScreen }) => {
             className="container flex flex-col mx-auto space-y-12"
           >
             <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-azulClaro">
-              <div className="space-y-2 col-span-full lg:col-span-1">
-                <label className="text-sm">Imagen</label>
-                <div className=" items-center space-x-2 justify-center text-center">
-                  <img src={logo} alt="" className=" w-full  " />
-                  <div className="p-3">
+              <div className=" col-span-full lg:col-span-1 justify-center ">
+                <div className="flex flex-col  h-full">
+                  <div className=" items-center space-x-2 justify-center text-center">
+                    <img src={logo} alt="" className=" w-full  " />
+                  </div>
+                  <div className="flex h-full justify-center items-center">
                     <button
                       type="button"
-                      className="px-4 py-2 rounded-3xl   inline-block bg-morado text-white"
+                      className="px-4 py-2 rounded-3xl  inline-block bg-morado text-white text-xl"
                     >
                       Cambiar
                     </button>
@@ -30,51 +33,48 @@ const AgregarServicio = ({ handleScreen }) => {
               </div>
               <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
                 <div className="col-span-full sm:col-span-3">
-                  <label className="text-sm">Nombre del Servicio</label>
+                  <label className="text-xl">Nombre del Servicio</label>
                   <input
                     id="nombre"
                     type="text"
-                    placeholder="Nombre del Servicio"
-                    className="w-full rounded-md focus:ring focus:ri focus:ri border-gray-700 text-gray-900 p-2"
+                    placeholder=""
+                    className="w-full rounded-md focus:ring focus:ri focus:ri border-gray-700 text-gray-900 p-2 "
                   />
                 </div>
                 <div className="col-span-full sm:col-span-3">
-                  <label className="text-sm">Precio</label>
+                  <label className="text-xl">Precio</label>
                   <input
                     id="precio"
                     type="text"
-                    placeholder="Precio"
-                    className="w-full rounded-md focus:ring focus:ri focus:ri border-gray-700 text-gray-900 p-2"
+                    className="w-full rounded-md focus:ring focus:ri focus:ri border-gray-700 text-gray-900 p-2 "
                   />
                 </div>
                 <div className="col-span-full sm:col-span-3">
-                  <label className="text-sm">Tipo del Servicio</label>
+                  <label className="text-xl">Tipo del Servicio</label>
                   <input
                     id="tipo"
                     type="email"
-                    placeholder="Tipo del Servicio"
-                    className="w-full rounded-md focus:ring focus:ri focus:ri border-gray-700 text-gray-900 p-2"
+                    className="w-full rounded-md focus:ring focus:ri focus:ri border-gray-700 text-gray-900 p-2 "
                   />
                 </div>
                 <div className="col-span-full">
-                  <label className="text-sm">Descripcion del Servicio</label>
+                  <label className="text-xl">Descripcion del Servicio</label>
                   <textarea
                     id="bio"
                     placeholder=""
-                    className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900 p-2"
+                    className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900 p-2 "
                   ></textarea>
                 </div>
                 <div className="col-span-full sm:col-span-2">
-                  <label className="text-sm">Duracion</label>
+                  <label className="text-xl">Duracion</label>
                   <input
                     id="duracion"
                     type="text"
-                    placeholder="Duracion"
                     className="w-full rounded-md focus:ring focus:ri focus:ri border-gray-700 text-gray-900 p-2"
                   />
                 </div>
                 <div className="col-span-full sm:col-span-2">
-                  <label className="text-sm">Numero Maximo de Personas</label>
+                  <label className="text-xl">Numero Maximo de Personas</label>
                   <input
                     id="max"
                     type="text"
@@ -82,46 +82,20 @@ const AgregarServicio = ({ handleScreen }) => {
                     className="w-full rounded-md focus:ring focus:ri focus:ri border-gray-700 text-gray-900 p-2"
                   />
                 </div>
-                <div className="col-span-full sm:col-span-2">
-                  <label className="text-sm">Numero</label>
-                  <input
-                    id="numero"
-                    type="text"
-                    placeholder="#"
-                    className="w-full rounded-md focus:ring focus:ri focus:ri border-gray-700 text-gray-900 p-2"
-                  />
+                <div className="col-span-full sm:col-span-2 relative">
+                  <button
+                    type="button"
+                    className="px-4 py-2 rounded-3xl   inline-block bg-morado text-white text-xl absolute bottom-0 w-full"
+                  >
+                    Editar
+                  </button>
                 </div>
               </div>
             </fieldset>
           </form>
         </section>
-        <div class="relative inline-block text-left">
-          <div>
-            <button
-              type="button"
-              class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              id="menu-button"
-              aria-expanded="true"
-              aria-haspopup="true"
-            >
-              Options
-              <svg
-                class="-mr-1 h-5 w-5 text-gray-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
-
-        </div>
       </div>
+      <Footer></Footer>
     </>
   );
 };
