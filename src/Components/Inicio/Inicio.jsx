87b5ad76a0193434b2eaeca4aa0e6e1a61b7ca3e
EvelynEscamilla <shieunwook@gmail.com>
2023-11-08 14:04:52 -0600

    correción

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -5,7 +5,6 @@ import MpImg1 from '../../assets/Inicio/Mp-img1.jpg'
 import MpImg2 from '../../assets/Inicio/Mp-img2.jpg'
 import MpImg3 from '../../assets/Inicio/Mp-img3.jpg'
 import LChatbot2 from '../../assets/logosolito.svg'
-import ChatbotClose from '../../assets/Inicio/delete-button.png'
 import React, { useState } from 'react';
 import './Inicio.css'
 import Navbar from "../Navbar/Navbar"
@@ -67,61 +66,6 @@ const Inicio = ({handleScreen}) => {
       <div class="bg-morado bg-opacity-70 text-white text-center p-2 absolute bottom-0 left-0 w-full">
         <p class="text-lg font-bold">Oxígeno</p>
       </div>
-      <br></br>
-      <div>
-        <div class="contenedor">
-          <div class="imagen-contenedor">
-              <div class="imagen-redonda hover:-translate-y-1 hover:scale-110 duration-200">
-                  <img src={MpImg1} alt="Imagen 1"></img>
-                  <div class="cuadro-morado">
-                      <p>Botox Facial</p>
-                  </div>
-              </div>
-              <div class="imagen-redonda hover:-translate-y-1 hover:scale-110 duration-200">
-                  <img src={MpImg2} alt="Imagen 2"></img>
-                  <div class="cuadro-morado">
-                      <p>Oxigeneo</p>
-                  </div>
-              </div>
-              <div class="imagen-redonda hover:-translate-y-1 hover:scale-110 duration-200">
-                  <img src={MpImg3} alt="Imagen 3"></img>
-                  <div class="cuadro-morado">
-                      <p>Carboxiterapia</p>
-                  </div>
-              </div>
-          </div>
-          {infoDiv == 1 ?
-          <img className=" fixed bottom-0 rounded-full bg-azulNav border-3 border-morado shadow-lg p-2 right-0 h-24 mx-2 my-4 hover:-translate-y-1 hover:scale-110 duration-200 " src={LChatbot2} alt="Mi Imagen Fija" onClick ={() => handleClick()}></img>
-          :
-
-          <div className=' z-50'>
-              <img className=" fixed bottom-[19rem] rounded-full p-2 right-0 w-12 mx-2 mb-[2rem] hover:-translate-y-1 hover:scale-110 duration-200 bg-azulClaro" src={ChatbotClose} alt="Mi Imagen Fija" onClick={() => handleClick1()}></img>
-              <div className='chatScreen fixed bottom-0 right-0 m-2 rounded-xl shadow-2xl border-3 h-[20rem] w-80 bg-azulNav border-morado text-white' >
-                <p className='bg-turqueza p-1 m-3 rounded-lg w-36 selection:bg-morado' >
-                  ¡Hola! ¿En qué puedo ayudarte?
-                </p>
-                <div className='Preg absolute bottom-0 bg-azulClaro rounded-b-xl w-full h-21'>
-                  <p className=' ml-1'>Preguntas Frecuentes:</p>
-                  <div className='flex flex-row pb-2'>
-                    <select className='pregFrec bg-azulClaro selection:bg-morado rounded-2xl p-1 w-48'>
-                      
-                        <option value="1">¿Donde se ubica la clinica?</option>
-                        <option value="2">¿Que servicios ofrecen?</option>
-                        <option value="3">¿Que metodos de pago se aceptan?</option>
-                      
-                    </select>
-                    <button className=' bg-morado hover:bg-[#6f789f] p-3 rounded-2xl text-lg w-full mx-2'>Enviar</button>
-                  </div>
-                  {/*   <p className=' border rounded-xl'>¿Donde se ubica la clinica?</p>
-                <p className=' border rounded-xl'>¿Que metodos de pago se aceptan?</p>
-                <p className=' border rounded-xl    '>¿Que servicios ofrecen?</p> */}
-                </div>
-
-              </div>
-            </div>
-     }
-          </div>
-
     </div>
     <div class="rounded-[10%] overflow-hidden w-[30%] h-96 relative hover:-translate-y-1 hover:scale-110 duration-200">
       <img src={MpImg3} alt="Imagen 3" class="w-full h-full object-cover"></img>
@@ -142,7 +86,6 @@ const Inicio = ({handleScreen}) => {
         <p class='border rounded-xl'>¿Dónde se ubica la clínica?</p>
         <p class='border rounded-xl'>¿Qué métodos de pago se aceptan?</p>
         <p class='border rounded-xl'>¿Qué servicios ofrecen?</p>
-
       </div>
     </div>
   }
