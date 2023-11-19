171290f04bccece5ce1ae38dd355691df0264acd
GonAvi <dgaduff@gmail.com>
2023-11-18 21:44:28 -0600

    Modificado el text form

--- src/Components/Forms/FormSignUp.jsx
+++ src/Components/Forms/FormSignUp.jsx
@@ -14,9 +14,10 @@ const FormSignUp = () => {
 
     return (
         <form onSubmit={handleSubmit} className="text-center">
-            <input onChange={handleFormDataChange} name='email' className=" w-[300px] h-10 bg-azulNav text-center rounded-full mb-4" type="email" placeholder="Correo electrónico"></input>
-            <br />
-            <input onChange={handleFormDataChange} name='password' className="w-[300px] h-10 bg-azulNav text-center  rounded-full mb-4" type="password" placeholder="Contraseña"></input>
+            <input onChange={handleFormDataChange} name='nombreCompleto' className=" w-[300px] h-10 bg-azulNav text-center rounded-full mb-4" type="text" placeholder="Nombre completo"/>
+            <input onChange={handleFormDataChange} name='telefono' className=" w-[300px] h-10 bg-azulNav text-center rounded-full mb-4" type="tel" placeholder="Numero de celular"/>
+            <input onChange={handleFormDataChange} name='email' className=" w-[300px] h-10 bg-azulNav text-center rounded-full mb-4" type="email" placeholder="Correo electrónico"/>
+            <input onChange={handleFormDataChange} name='password' className="w-[300px] h-10 bg-azulNav text-center  rounded-full mb-4" type="password" placeholder="Contraseña"/>
             <input onChange={handleFormDataChange} name='passwordConfirmation' className="w-[300px] h-10 bg-azulNav text-center  rounded-full mb-4" type="password" placeholder="Confirmar Contraseña"></input>
             <p className="texto-fuente">
                 Utiliza mínimo ocho caracteres combinados
@@ -25,16 +26,8 @@ const FormSignUp = () => {
                 Letras, números y símbolos
             </p>
             <div className="mt-4">
-                <input
-                    type="submit"
-                    value="Cancelar"
-                    className="text-[20px] text-white bg-morado m-2 px-4 py-2  rounded-full cursor-pointer float-left"
-                ></input>
-                <input
-                    type="submit"
-                    value="Crear Cuenta"
-                    className="text-[20px] text-white bg-morado m-2 px-4 py-2 rounded-full cursor-pointer float-right"
-                ></input>
+                <button type="submit" className="text-[20px] text-white bg-morado m-2 px-4 py-2  rounded-full cursor-pointer float-left">Cancelar</button>
+                <button type="submit" className="text-[20px] text-white bg-morado m-2 px-4 py-2 rounded-full cursor-pointer float-right">Crear cuenta</button>
             </div>
         </form>
     )
