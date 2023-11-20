97f2408639470b18e52e10c65fad7d872bc50cd7
AngheloX09 <linkg4f@gmail.com>
2023-11-20 17:51:04 -0600

    Login responsivo listo

--- src/routes/Login2.jsx
+++ src/routes/Login2.jsx
@@ -11,19 +11,19 @@ const Login2 = () => {
         className=" w-5/6 h-screen justify-end flex bg-cover bg-center"
         style={divStyle}
       >
-        <div className="w-1/2 bg-azulNav bg-opacity-90  text-center p-8">
-          <img className="w-1/2 mx-auto py-7 " src="Images/Login/Lily-hor.png"></img>
+        <div className="w-100% bg-azulNav bg-opacity-75 lg:bg-opacity-90 text-center p-8 lg:w-1/2">
+          <img className="lg:w-1/2 mx-auto py-7 w-3/4 " src="Images/Login/Lily-hor.png"></img>
           <p className=" text-3xl py-3">Inicio de Sesión</p>
           <div className="py-3">
             <input
-              className="text-3xl py-3 w-3/4 text-center rounded-3xl"
+              className="text-3xl py-3 lg:w-3/4 w-full text-center rounded-3xl"
               type="text"
               placeholder="Correo electrónico"
             ></input>
           </div>
           <div className="py-3">
             <input
-              className=" text-3xl py-3 w-3/4 text-center rounded-3xl"
+              className=" text-3xl py-3 lg:w-3/4 w-full text-center rounded-3xl"
               type="text"
               placeholder="Contraseña"
             ></input>
