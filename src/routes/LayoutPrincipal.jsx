05bcb7ad4e4a17102e9adc06d56e398aa058ada9
EvelynEscamilla <shieunwook@gmail.com>
2023-11-21 16:17:15 -0600

    navbar admin

--- src/routes/LayoutPrincipal.jsx
+++ src/routes/LayoutPrincipal.jsx
@@ -1,12 +1,13 @@
 
 import Footer from "../Components/Footer/Footer"
 import Navbar from "../Components/Navbar/Navbar"
+import NavbarAdministrador from "../Components/Navbar/NavbarAdministrador"
 import { Outlet } from "react-router-dom"
 
 const LayoutPrincipal = () => {
   return (
     <>
-      <Navbar />
+      <NavbarAdministrador />
       <div className="w-full min-h-screen bg-white">
         <Outlet />
       </div>
