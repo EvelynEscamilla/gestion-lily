d028108f4514dfac89086f788d774e4918cf4d18
Rodrik-09 <becerra24502@gmail.com>
2023-11-23 02:13:27 -0600

    Forms realizados, mejora en add/modif productos

--- src/routes/LayoutPrincipal.jsx
+++ src/routes/LayoutPrincipal.jsx
@@ -1,6 +1,6 @@
 
 import Footer from "../Components/footer/Footer"
-import Navbar from "../Components/navbar/NavbarCliente"
+import Navbar from "../Components/navbar/Navbar"
 import { Outlet } from "react-router-dom"
 
 const LayoutPrincipal = () => {
