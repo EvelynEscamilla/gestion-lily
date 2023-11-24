9221ed9ff633445508d8e0aa3a1dc3d5074d3702
Omar Garcia <l20120988@morelia.tecnm.mx>
2023-11-24 13:19:12 -0600

    Primera Parte del Calendario

---
+++ src/Components/calendar/Calendar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { esES } from "@mui/x-date-pickers/locales";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+
+const theme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: "#8893c2",
+        light: "#8893c2",
+        dark: "#8893c2",
+        contrastText: "#fff",
+      },
+    },
+  },
+  esES
+);
+const Calendar = () => {
+  return (
+    <>
+    <div className="  flex justify-center lg:p-10 sm:p-5 py-3">
+      <div className="w-fit  rounded-lg border-2 border-azul  bg-azulNav  flex flex-col text-center  ">
+        <div className=" bg-azul">
+          <p className=" font-medium text-xl p-3">
+            Selecciona la fecha de tu cita
+          </p>
+        </div>
+        <ThemeProvider theme={theme} >
+          <LocalizationProvider dateAdapter={AdapterDayjs} >
+            <StaticDatePicker
+              sx={{
+                bgcolor: "#EDF8F8"
+                
+              }}
+              slotProps={{
+                actionBar: {
+                  actions: ["today"],
+                },
+                toolbar: {
+                  hidden: true,
+                },
+              }}
+            />
+          </LocalizationProvider>
+        </ThemeProvider>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default Calendar;
--- src/routes/Calendario.jsx
+++ src/routes/Calendario.jsx
@@ -1,16 +1,13 @@
-import React from "react";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import * as React from "react";
+import Calendar from "../Components/calendar/Calendar";
 
 const Calendario = () => {
   return (
     <>
-      <div className=" w-full lg:flex sm:block">
-        <div className=" lg:w-1/2  bg-azulNav">
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar />
-          </LocalizationProvider>
+      <div className=" w-full lg:flex sm:block justify-center items-center min-h-screen ">
+        <div className=" lg:w-1/2 ">
+          
+          <Calendar/>
         </div>
         <div className=" lg:w-2/6 bg-blue-200">Calendario</div>
         <div className="lg:w-1/6 bg-slate-400">Calendario</div>
