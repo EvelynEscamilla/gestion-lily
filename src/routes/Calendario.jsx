f6541a148449e8259d83fedd620020741fcf0462
Omar Garcia Alvarez <l20120988@morelia.tecnm.mx>
2023-11-25 19:15:45 -0600

    calendario

--- src/routes/Calendario.jsx
+++ src/routes/Calendario.jsx
@@ -1,16 +1,42 @@
-import * as React from "react";
-import Calendar from "../Components/calendar/Calendar";
+import React, { useState } from "react";
+import useForm from "../hooks/useForm";
+import CalendarComp from "../Components/calendar/CalendarComp";
+
 
 const Calendario = () => {
+  const { formData, handleFormDataChange } = useForm();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <>
-      <div className=" w-full lg:flex sm:block justify-center items-center min-h-screen ">
-        <div className=" lg:w-1/2 ">
+      <div >
+      <CalendarComp></CalendarComp>
+        <form onSubmit={(e) =>{e.preventDefault}} className=" w-full lg:flex sm:block justify-center items-center min-h-screen ">
+          <div className=" lg:w-1/2 ">
           
-          <Calendar/>
-        </div>
-        <div className=" lg:w-2/6 bg-blue-200">Calendario</div>
-        <div className="lg:w-1/6 bg-slate-400">Calendario</div>
+          </div>
+          <div className=" lg:w-2/6 bg-blue-200">
+            {" "}
+            <input
+              name="Servicios"
+              type="text"
+              onChange={handleFormDataChange}
+              placeholder="Servicios"
+            />
+          </div>
+          <div className="lg:w-1/6 bg-slate-400">
+            {" "}
+            <input
+              name="Hora"
+              type="text"
+              onChange={handleFormDataChange}
+              placeholder="Hora"
+            />
+          </div>
+          <button type="submit">WAOS</button>
+        </form>
       </div>
     </>
   );
