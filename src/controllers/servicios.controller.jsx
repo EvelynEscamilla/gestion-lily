0fa7c970a2c7efe991e058afbc9d25bdbfc2c6ae
Rodrik-09 <becerra24502@gmail.com>
2023-12-04 12:27:15 -0600

    coms

--- src/controllers/servicios.controller.jsx
+++ src/controllers/servicios.controller.jsx
@@ -16,7 +16,7 @@ import { addDoc, collection, getDocs, doc, updateDoc, deleteDoc, getDoc } from '
 import { db, storage } from '../firebase'
 import { getDownloadURL, ref, uploadBytes, deleteObject } from 'firebase/storage'
 
-
+//hola
 
 const reference = "Servicios";
 const storageReference = "Servicios";
